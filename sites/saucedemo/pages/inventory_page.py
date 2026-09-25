e157bb0ef5ba0cd7884eb6f4823b32ffe235430a
anyuger <anton.ger@outlook.com>
2026-09-24 21:22:00 -0700

    Review agent tests: move sort locators into InventoryPage, check full sort order

--- sites/saucedemo/pages/inventory_page.py
+++ sites/saucedemo/pages/inventory_page.py
@@ -10,6 +10,9 @@ class InventoryPage:
         self.inventory_items = page.locator(".inventory_item")
         self.cart_icon = page.locator(".shopping_cart_link")
         self.cart_badge = page.locator(".shopping_cart_badge")
+        self.sort_dropdown = page.locator("[data-test='product-sort-container']")
+        self.item_names = page.locator("[data-test='inventory-item-name']")
+        self.item_prices = page.locator("[data-test='inventory-item-price']")
 
     def get_item_count(self) -> int:
         # count() does not wait, so wait for the product list to render first
@@ -27,3 +30,16 @@ class InventoryPage:
 
     def go_to_cart(self):
         self.cart_icon.click()
+
+    def sort_by(self, option: str):
+        """option is the dropdown's value: az, za, lohi or hilo"""
+        self.sort_dropdown.select_option(option)
+
+    def get_item_names(self) -> list:
+        self.inventory_items.first.wait_for()
+        return self.item_names.all_text_contents()
+
+    def get_item_prices(self) -> list:
+        """Prices as numbers, e.g. [7.99, 9.99, ...]"""
+        self.inventory_items.first.wait_for()
+        return [float(price.replace("$", "")) for price in self.item_prices.all_text_contents()]
